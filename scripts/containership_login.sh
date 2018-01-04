eff29d512ffe6b1ff60b5405e27bf93d40bfe88b
Matt Kelly <matthew.joseph.kelly@gmail.com>
2018-01-04 15:23:39 -0500

    Add login script for use in authorized_keys

---
+++ scripts/containership_login.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+USER=$1
+USER_HOME=/opt/containership/home/$USER
+
+if [ ! -d $USER_HOME ]; then
+    echo "Creating user home at $USER_HOME"
+    sudo mkdir $USER_HOME
+fi
+
+if ! getent passwd $USER > /dev/null 2>&1; then
+    echo "Creating user $USER"
+    sudo useradd -d $USER_HOME -s /bin/bash $USER
+
+    echo "Fixing ownership of $USER_HOME"
+    sudo chown $USER:$USER $USER_HOME
+fi
+
+echo "Dropping into user shell"
+sudo su - $USER
