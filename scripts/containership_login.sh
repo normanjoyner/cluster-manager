900f5b5c8a781181960ed82bd48122aee1c016e0
Matt Kelly <matthew.joseph.kelly@gmail.com>
2018-01-10 12:25:54 -0500

    Add passwordless sudo for created users

--- scripts/containership_login.sh
+++ scripts/containership_login.sh
@@ -3,17 +3,19 @@
 USER=$1
 USER_HOME=/etc/containership/home/$USER
 
-if [ ! -d $USER_HOME ]; then
-    echo "Creating user home at $USER_HOME"
-    sudo mkdir $USER_HOME
+# Bail if no args
+if [[ $# -eq 0 ]]; then
+    echo "ERROR: User argument required"
+    exit 1
 fi
 
+# Create user if it doesn't exist
 if ! getent passwd $USER > /dev/null 2>&1; then
     echo "Creating user $USER"
     sudo useradd -d $USER_HOME -s /bin/bash $USER
 
-    echo "Fixing ownership of $USER_HOME"
-    sudo chown $USER:$USER $USER_HOME
+    echo "Adding passwordless sudo for $USER"
+    sudo sh -c "echo '$USER ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/$USER"
 fi
 
 echo "Dropping into user shell"
