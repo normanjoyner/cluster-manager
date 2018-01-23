dbaaed6ed235ce32c11679c61302df2d69946913
Matt Kelly <matthew.joseph.kelly@gmail.com>
2018-01-23 13:24:10 -0500

    Add build info to metadata endpoint and remove nodes

--- internal/buildinfo/build_info.go
+++ internal/buildinfo/build_info.go
@@ -6,23 +6,42 @@ import (
 
 // These variables should be initialized by ldflags
 var (
-	// GitDescribe is the git describe for this build
+	// GitDescribeString is the git describe for this build as set by ldflags
 	GitDescribe string
-	// GitCommit is the git commit for this build
+	// GitCommitString is the git commit for this build as set by ldflags
 	GitCommit string
 )
 
+// Spec represents the build info for the running binary
+type Spec struct {
+	// GitDescribe is the git describe for this build
+	// It should be a clean git tag for a real release
+	GitDescribe string `json:"git_describe"`
+	// GitCommit is the git commit for this build
+	GitCommit string `json:"git_commit"`
+}
+
+func init() {
+	// If variables weren't initialized by ldflags, set them to
+	// a dummy value
+	if GitDescribe == "" {
+		GitDescribe = "unknown"
+	}
+
+	if GitCommit == "" {
+		GitCommit = "unknown"
+	}
+}
+
+// Get returns a copy of the build info
+func Get() Spec {
+	return Spec{
+		GitDescribe: GitDescribe,
+		GitCommit:   GitCommit,
+	}
+}
+
 // String returns a string representation of the build info
 func String() string {
-	describe := GitDescribe
-	if describe == "" {
-		describe = "<unknown git describe>"
-	}
-
-	commit := GitDescribe
-	if commit == "" {
-		commit = "<unknown git commit>"
-	}
-
-	return fmt.Sprintf("%s (%s)", describe, commit)
+	return fmt.Sprintf("%s (%s)", GitDescribe, GitCommit)
 }
--- internal/server/handlers/metadata.go
+++ internal/server/handlers/metadata.go
@@ -4,57 +4,30 @@ import (
 	"net/http"
 	"time"
 
-	corev1 "k8s.io/api/core/v1"
-
+	"github.com/containership/cloud-agent/internal/buildinfo"
 	"github.com/containership/cloud-agent/internal/envvars"
-	"github.com/containership/cloud-agent/internal/k8sutil"
 )
 
-type node struct {
-	corev1.NodeSystemInfo
-	NodeID string `json:"nodeID"`
+type containershipClusterMetadata struct {
+	ClusterID      string         `json:"cluster_id"`
+	OrganizationID string         `json:"organization_id"`
+	BuildInfo      buildinfo.Spec `json:"build_info"`
 }
 
-type containershipClusterMetadata struct {
-	ClusterID      string `json:"cluster_id"`
-	OrganizationID string `json:"organization_id"`
-}
 type metadata struct {
 	Containership containershipClusterMetadata `json:"containership"`
 	Timestamp     time.Time                    `json:"timestamp"`
-	Nodes         []node
-}
-
-func getNodes() ([]node, error) {
-	nodes, err := k8sutil.API().GetNodes()
-	if err != nil {
-		return nil, err
-	}
-
-	allNodes := make([]node, 0)
-	for _, n := range nodes.Items {
-		allNodes = append(allNodes, node{n.Status.NodeInfo, "nodeid"})
-	}
-
-	return allNodes, nil
 }
 
 // Get returns Containership and node metadata
 func (meta *Metadata) Get(w http.ResponseWriter, r *http.Request) {
-	nodes, err := getNodes()
-
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-
 	m := &metadata{
 		Containership: containershipClusterMetadata{
 			ClusterID:      envvars.GetClusterID(),
 			OrganizationID: envvars.GetOrganizationID(),
+			BuildInfo:      buildinfo.Get(),
 		},
 		Timestamp: time.Now(),
-		Nodes:     nodes,
 	}
 
 	respondWithJSON(w, http.StatusOK, m)
