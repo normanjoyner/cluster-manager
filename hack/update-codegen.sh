d55bd71bd23ec9670ebd353f417c3d376a639e1f
Matt Kelly <matthew.joseph.kelly@gmail.com>
2018-06-28 15:47:25 -0400

    Move non-prod scripts to hack dir, add codegen verify
    This also reworks the generate and verify scripts to be *very* close to
    the scripts available in other Kubernetes projects. This will make it
    trivial to update them in the future if needed.

---
+++ hack/update-codegen.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+# TODO uncomment when gengo workaround is removed
+#set -o pipefail
+
+# TODO remove these workarounds
+go get -u k8s.io/gengo || true
+rm -rf ./vendor/github.com/golang/glog
+rm -rf ./vendor/github.com/spf13/pflag
+
+SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
+CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
+
+${CODEGEN_PKG}/generate-groups.sh all \
+  github.com/containership/cloud-agent/pkg/client github.com/containership/cloud-agent/pkg/apis \
+  "containership.io:v3 provision.containership.io:v3"
